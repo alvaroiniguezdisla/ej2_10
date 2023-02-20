efdfbeace0c0a2940c68bda4746daf46b79e731c
Álvaro <ainiguezdislaa@gmail.com>
2023-02-20 15:41:30 +0100

    ej10 terminado

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,2 +1,15 @@
-package aplicacion;public class Principal {
+package aplicacion;
+import java.util.ArrayList;
+import java.util.Scanner;
+import dominio.ej2_10;
+
+public class Principal {
+    public static void main(String[] args) {
+        ArrayList listapares=new ArrayList();
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Dime un número");
+        int num=sc.nextInt();
+        System.out.println(ej2_10.pares(listapares,num));
+
+    }
 }
--- src/dominio/ej2_10.java
+++ src/dominio/ej2_10.java
@@ -1,2 +1,26 @@
-package dominio;public class ej2_10 {
+package dominio;
+import java.util.ArrayList;
+
+public class ej2_10 {
+
+
+
+    public static ArrayList pares(ArrayList listapares,int n) {
+        if (n==2) {
+            listapares.add(n);
+            return listapares;
+        }else {
+            if(n%2==0) {
+                listapares.add(n);
+                return pares(listapares,n-1);
+            }else {
+                return pares(listapares,n-1);
+
+            }
+        }
+
+
+
+    }
+
 }
